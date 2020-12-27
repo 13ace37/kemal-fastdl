04ce2d5a076fda0223a494904015176ee0b05d88
13ace37 <github@xace.ch>
2020-12-27 16:09:15 +0100

    added custom port support
    moved -p -> -d and -p is for the port now

--- src/kemal-fastdl.cr
+++ src/kemal-fastdl.cr
@@ -1,6 +1,7 @@
 require "kemal"
 require "option_parser"
 server_path = ""
+port = "36949"
 
 module Kemal::Fastdl
 	VERSION = "0.0.1"
@@ -15,8 +16,11 @@ module Kemal::Fastdl
 			puts parser
 			exit
 		end
-		parser.on "-p PATH", "--path=PATH", "Starts server with given path" do |path|
-			server_path = path
+		parser.on "-d DIR", "--dir=DIR", "Starts server with given dir" do |dir|
+			server_path = dir
+		end
+		parser.on "-p PORT", "--port=PORT", "Starts server with custom port" do |cport|
+			port = cport
 		end
 	end
 	unless server_path.empty?
@@ -32,6 +36,9 @@ module Kemal::Fastdl
 				env.response.respond_with_status(404)
 			end
 		end
-		Kemal.run
+		Kemal.run do |config|
+			server = config.server.not_nil!
+			server.bind_tcp "0.0.0.0", port.to_i32, reuse_port: true
+		end
 	end
 end
